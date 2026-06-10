67c6600ea6ba5aca4c4c7d356e457fe6a40bb5d8
ionut <rautuionut7@gmail.com>
2026-06-10 13:26:18 +0300

    finished

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  let newArr = []
+
+  for (const num of arr) {
+    if (!args.includes(num)) {
+      newArr.push(num)
+    }
+  }
+
+  return newArr
 };
 
 // Do not edit below this line
