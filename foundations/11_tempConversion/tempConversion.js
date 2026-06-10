ebefa782362049866e8df47f5061aa14db94fe52
ionut <rautuionut7@gmail.com>
2026-06-10 13:53:47 +0300

    finished

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const conv = (temp - 32) / (9 / 5)
+
+  return Number.isInteger(conv) ? conv : Number(conv.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const conv = (temp * (9 / 5) + 32)
+
+  return Number.isInteger(conv) ? conv : Number(conv.toFixed(1))
 };
 
 // Do not edit below this line
