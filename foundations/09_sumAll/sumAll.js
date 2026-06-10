ce161786d466dfc36a3c095e78c5b35762685f03
ionut <rautuionut7@gmail.com>
2026-06-10 13:36:40 +0300

    finished

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  const array = []
+  
+  if (a < 0 || b < 0) {
+    return 'ERROR'
+  } if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return 'ERROR'
+  }
 
+  for (let i = a < b ? a : b; a < b ? i <= b : i <= a; i++) {
+    array.push(i)
+  }
+
+  return array.reduce((acc, curr) => acc + curr, 0)
 };
 
 // Do not edit below this line
